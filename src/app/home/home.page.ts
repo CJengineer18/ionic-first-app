c86cf65cddcb189280101355cbe055eae17c6b58
CJengineer18 <cjjd18.e2@gmail.com>
2019-12-13 11:39:08 -0500

    The APP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  vengadores = [
+    {
+      nombre: 'Tony',
+      imagen: 'assets/imagenes/ironman.jpg',
+      audio: 'assets/audio/voztony.mp3'
+    },
+    {
+      nombre: 'Thor',
+      imagen: 'assets/imagenes/thor.jpg',
+      audio: 'assets/audio/vozthor.mp3'
+    },
+    {
+      nombre: 'Spider-Man',
+      imagen: 'assets/imagenes/spyderman.jpg',
+      audio: 'assets/audio/vozspyderman.mp3'
+    },
+    {
+      nombre: 'Capi',
+      imagen: 'assets/imagenes/capi.jpg',
+      audio: 'assets/audio/vozcapi.mp3'
+    }
+  ];
+
+  constructor() { }
+
+  reproducirVoz(vengador) {
+    console.log(vengador);
+    let voz = new Audio();
+
+    voz.src = vengador.audio;
+    voz.load();
+    voz.play();
+  }
 
 }
